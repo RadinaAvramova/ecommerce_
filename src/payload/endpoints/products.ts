33e95ddec9dbeb8ba6a26572a923d18ac1cf0c70
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-06-29 20:53:20 +0300

    Update products.ts

--- src/payload/endpoints/products.ts
+++ src/payload/endpoints/products.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe'
 import { checkRole } from '../collections/Users/checkRole'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2022-08-01',
+  apiVersion: '',
 })
 
 const logs = process.env.LOGS_STRIPE_PROXY === '1'
